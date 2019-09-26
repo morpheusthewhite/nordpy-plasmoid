3070f421ce28662c6ba72bda54ee8429fb16b194
morpheusthewhite <zffromGerace@hotmail.it>
2019-09-26 10:13:28 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo "Building and installing plugin"
+mkdir -p plugin/build
 cd plugin/build
-qmake .. && make && sudo make install || (echo "Failed building plugin" && exit 1)
+qmake .. && make && sudo make install || { echo "Failed building plugin"; exit 1; }
 
 echo "Building and installing plasmoid"
-cd ../../build
-pwd
-cmake .. && make && sudo make install || (echo "Failed building plasmoid" && exit 1)
+cd ../..
+mkdir -p build
+cd build
+cmake .. && make && sudo make install || { echo "Failed building plasmoid"; exit 1; }
